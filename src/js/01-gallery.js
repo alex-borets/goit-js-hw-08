a78a6f9fc554fe1df6ef61ad562823db2d76fdf6
Alex Borets <alexborets2812@gmail.com>
2022-01-31 11:41:12 +0200

    task 1 check

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}" 
+    alt="${description}" 
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox(`.gallery a`, {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
