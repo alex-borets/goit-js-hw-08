edc7b25388718d402b6e920a57c6c8076e3d0147
Alex Borets <alexborets2812@gmail.com>
2022-01-31 14:54:03 +0200

    task 2 rdy

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+let playerSeconds;
+const currentTimeStart = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTimeStart);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    playerSeconds = data.seconds;
+    localStorage.setItem('videoplayer-current-time', playerSeconds);
+    console.log(playerSeconds);
+  }, 1000),
+);
